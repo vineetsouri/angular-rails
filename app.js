7e3932eff97a0b2cc041da86283955fb3da7ff2c
vineetsouri <vineetsouri72@gmail.com>
2016-05-17 12:02:12 +0530

    post comments implemented in angular

--- app.js
+++ app.js
@@ -28,6 +28,15 @@ angular.module('flapperNews', ['ui.router'])
 
 .controller('PostsCtrl', ['$scope','$stateParams','posts', function ($scope,$stateParams,posts) {
     $scope.posts = posts.posts[$stateParams.id];
+    $scope.addComment = function(){
+      if ($scope.body == '') {return;};
+      $scope.post.comments.push({
+        body: $scope.body,
+        author: 'user',
+        upvotes: 0
+      });
+      $scope.body = '';
+    }
 }])
 
 //...........factory starts............
